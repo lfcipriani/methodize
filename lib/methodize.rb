49af2a18f12179e78143653e835e05dbf3afdbbb
Marcelo Manzan <marmanzan@abril.com.br>
2012-02-10 09:31:15 -0200

    improvements on calculations and implicit calls

--- lib/methodize.rb
+++ lib/methodize.rb
@@ -16,7 +16,7 @@ module Methodize
   end
 
   def []=(key, value)
-    __free_method__(key) if !self.keys.include?(key) && self.public_methods.include?(@@key_coerce.call(key))
+    __free_method__(key) if !keys.include?(key) && public_methods.include?(@@key_coerce.call(key))
     super(key,value)
   end
 
@@ -24,10 +24,10 @@ module Methodize
     method_name = name.to_s
     if method_name[-1,1] == '='
       method_name = method_name.chop
-      self.key?(method_name) ? key = method_name : key = method_name.to_sym
+      key = key?(method_name) ? method_name : method_name.to_sym
       self[key] = args[0]
     else
-      self.key?(method_name) ? key = method_name : key = method_name.to_sym
+      key = key?(method_name) ? method_name : method_name.to_sym
       self[key]
     end
   end
@@ -37,8 +37,10 @@ module Methodize
   # to access the value of key "size".
   # you still can access the old method with __[method_name]__
   def __free_method__(sym)
-    self.__metaclass__.send(:alias_method, "__#{sym.to_s}__".to_sym, sym) unless self.respond_to?("__#{sym.to_s}__")
-    self.__metaclass__.send(:define_method, sym) { method_missing(sym.to_s) }
+    __sym__ = "__#{sym}__"
+    __metaclass__ = self.__metaclass__
+    __metaclass__.send(:alias_method, __sym__.to_sym, sym) unless self.respond_to?(__sym__)
+    __metaclass__.send(:define_method, sym) { method_missing(sym.to_s) }
     self
   end
 
